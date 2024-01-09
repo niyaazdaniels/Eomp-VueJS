efb3f75f5f3722116819228d42e5358e227567b7
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2024-01-09 08:34:22 +0200

    Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,42 +1,67 @@
 <template>
-    <div class="card text-center" style="max-width: 750px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img :src="$store.state.home[0].profile-image" class="img-fluid rounded-start">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{ $store.state.home[0].intro }}</h5>
-        <p class="card-text">{{ $store.state.home[0].subintro }}</p>
-        <p class="card-text"><small class="text-body-secondary"></small></p>
-      </div>
+  <div class="card text-center" style="max-width: 1000px;">
+<div class="row g-0">
+  <div class="col-md-4 bg-dark rounded">
+    <img :src="$store.state.home[0].image" class="img-fluid rounded-start">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h3 class="card-title">{{ $store.state.home[0].intro }}</h3>
+      <p class="card-text">{{ $store.state.home[0].subintro }}</p>
+      <p class="card-text">
+<a href="https://github.com/niyaazdaniels" target="_blank"><i class="fa-brands fa-github"></i></a>
+<a href="https://twitter.com/daniels_niyaaz" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+<a href="https://www.linkedin.com/in/niyaaz-daniels-6694a7129/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+</p>
+<a href="/assets/Niyaaz Daniels (Updated).pdf" target="_blank"><button class="btn btn-dark text-uppercase">Resume</button></a>
     </div>
   </div>
 </div>
- </template>
- 
- <script>
- 
- export default {
-  methods: {
-
-  },
-  fetchHome(){
-   return this.$store.dispatch('fetchHome')
-  },
- mounted(){
-  this.fetchHome
- }
+</div>
+</template>
+<script>
+export default {
+methods: {
+},
+fetchHome(){
+ return this.$store.dispatch('fetchHome')
+},
+mounted(){
+this.fetchHome
 }
- </script>
- <style scoped>
+}
+</script>
+<style scoped>
 .card{
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  height: 50vh;
-  text-align: center;
-  top: 100px;
-  left: 30%;
+text-align: center;
+top: 150px;
+left: 20%;
+padding: 10px
 }
- </style>
+h3 {
+font-size: 4rem;
+}
+p {
+font-size: 1.5rem;
+}
+i {
+color: black;
+padding: 10px;
+}
+@media (max-width:300px){
+.card{
+position: relative;
+top: 16vh;
+left: 2%;
+width: 290px;
+}
+img {
+height: 350px;
+width: auto;
+}
+i {
+color: black;
+padding: 5px;
+}
+}
+</style>
